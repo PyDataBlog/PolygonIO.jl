672dfbefd38fd367b1ef8f707868883622079581
Documenter.jl <documenter@juliadocs.github.io>
2021-06-08 20:33:52 +0000

    build based on 7d33e63

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PolygonIO","category":"page"},{"location":"#PolygonIO","page":"Home","title":"PolygonIO","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PolygonIO.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PolygonIO]","category":"page"}]
+}
